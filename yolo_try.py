17b2d1d5a924aef4f76c11bf95b115761b641b24
aussitot <contact@aussitot.net>
2024-05-08 17:13:11 +0200

    Update yolo_try.py
    j'ai enlevé ta ligne

--- yolo_try.py
+++ yolo_try.py
@@ -4,7 +4,6 @@ model = YOLO('yolov8m')
 
 results = model.predict('videos/video1.mp4', save=True)
 print(results[0])
-print('test')
 print('===================')
 for box in results[0].boxes:
     print(box)

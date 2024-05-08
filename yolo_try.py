cc2d0afd8e798bb56aa8801c3a31ca686cdcb2ba
JohanPETIT <johan.petit@edu.esiee.fr>
2024-05-08 17:29:57 +0200

    Update yolo_try.py

--- yolo_try.py
+++ yolo_try.py
@@ -4,7 +4,6 @@ model = YOLO('yolov8m')
 
 results = model.predict('videos/video1.mp4', save=True)
 print(results[0])
-print('pro')
 print('===================')
 for box in results[0].boxes:
     print(box)

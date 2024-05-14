edcebab660dcf057b96cf9f0d422823e92d72b53
JohanPETIT <johan.petit@edu.esiee.fr>
2024-05-14 10:13:33 +0200

    changement useless

--- Tracking/main.py
+++ Tracking/main.py
@@ -3,16 +3,16 @@ from trackers import Tracker
 
 def main():
  # On lit la vidéo en entrée
- video_frames = read_video('input_videos/video1.mp4')
+ video_frames = read_video('Tracking/input_videos/video1.mp4')
 
  # On instancie le Tracker
- tracker = Tracker('modeles/best.pt')
+ tracker = Tracker('Tracking/modeles/best.pt')
 
  tracks = tracker.get_objects_tracks(video_frames)
 
 
  # On enregistre la vidéo une fois les modifs apportées
- save_video(video_frames, 'output_videos/video1.avi')
+ save_video(video_frames, 'Tracking/output_videos/video1.avi')
 
 if __name__ == '__main__': # Fait fonctionner le main
  main()

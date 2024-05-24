11ab8804162ec74dc45485b42bf48b59fedea24f
JohanPETIT <johan.petit@edu.esiee.fr>
2024-05-24 18:32:29 +0200

    importation du local

---
+++ Local_FootTracker/graphic_interface/__init__.py
@@ -0,0 +1,1 @@
+from .interface import plot_page
---
+++ Local_FootTracker/graphic_interface/interface.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from plotly_football_pitch import make_pitch_figure, PitchDimensions, SingleColourBackground
+
+
+
+def plot_page():
+
+    # On dessine le truc pour sélectionner
+    option = st.selectbox("Quelle statistique vous intéresse ?", ("Possession", "Autre", "Autre"))
+
+    # On dessine le terrain
+    dimensions = PitchDimensions()
+    fig = make_pitch_figure(dimensions, pitch_background=SingleColourBackground("#74B72E"))
+    st.plotly_chart(fig)

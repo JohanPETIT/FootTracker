5ada30a99a29f1f046e566d4d1b503781d611396
JohanPETIT <johan.petit@edu.esiee.fr>
2024-06-21 17:48:27 +0200

    fix rename

--- Server_FootTracker/main.py
+++ Server_FootTracker/main.py
@@ -105,7 +105,6 @@ class MyApp():
 
     # Supprime tous les fichiers associés à une vidéo
     def delete_file(self, file=None):
-        os.remove(self.local_input_dir_path+file)
         os.remove(self.local_output_dir_path+file)
         os.remove('tracks_files/tracks_'+file[6:-4]+'.pkl')
         os.remove('events_files/events_'+file[6:-4]+'.pkl')

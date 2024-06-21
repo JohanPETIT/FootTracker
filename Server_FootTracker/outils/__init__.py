658b5f8ce6915023383c3ec9efebccfb14d7920c
JohanPETIT <johan.petit@edu.esiee.fr>
2024-06-21 14:53:47 +0200

    ajout server_foottracker

---
+++ Server_FootTracker/outils/__init__.py
@@ -0,0 +1,4 @@
+from .outils_bbox import get_center_bbox, get_width_bbox, distance, measure_xy_distance, get_foot_position
+from .team_colors import get_team_colors
+from .lecture_videos import save_video
+from .communication_ssh import get_tracks_and_events, send_new_video
